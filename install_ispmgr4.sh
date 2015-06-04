46adc33c0ed4b6d9241947faa91302f1542f087c
Limman <limman@tut.by>
2015-06-05 02:37:12 +0300

    Update install_ispmgr4.sh

--- install_ispmgr4.sh
+++ install_ispmgr4.sh
@@ -52,6 +52,30 @@ if [ $OSver -eq "10" ]; then
     cp /usr/ports/INDEX-10 /usr/ports/INDEX-9
 fi
 
+echo "Installing new pkg_info..."
+if [ ! -d "/var/db/pkgdb" ]; then  
+    mv /var/db/pkg /var/db/pkgdb
+    mkdir /var/db/pkg
+
+    if [ ! -f "/usr/local/etc/pkg.conf" ]; then
+        cp /usr/local/etc/pkg.conf.sample /usr/local/etc/pkg.conf
+    fi
+
+    sed -i '' 's/\#PKG_DBDIR/PKG_DBDIR/g' /usr/local/etc/pkg.conf
+    sed -i '' 's/\/var\/db\/pkg/\/var\/db\/pkgdb/g' /usr/local/etc/pkg.conf
+
+    if [ -f "/usr/sbin/pkg_info" ]; then
+        mv /usr/sbin/pkg_info /usr/sbin/pkg_info.orig
+    fi
+
+    fetch -o /usr/sbin/pkg_info -q --no-verify-peer "https://github.com/Limman/freebsd-ispmgr4/raw/master/pkg_info"
+    chmod 555 /usr/sbin/pkg_info
+    echo "Building old packages database..."
+    /usr/sbin/pkg_info update
+else
+    echo "New pkg_info is already installed!"    
+fi
+
 echo "Downloading ISPmanager..."
 fetch -o /tmp/install.tgz -q "http://download.ispsystem.com/FreeBSD-9.3/$arch/ISPmanager-$1/install.tgz"
 mkdir /usr/local/ispmgr
